4da36d8ec2e20771b73bca368947d69823e284d3
enriquetrujillo4 <enrique.trujillo@ucb.edu.bo>
2025-04-10 09:57:21 -0400

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,51 @@
+#crear una var#
+saludo = "Hola mundo !!!"
+print(saludo)
+
+
+#suma de número#
+
+num1 = 10
+num2 = 20 
+
+suma = num1 + num2 
+
+print( "el resultado de la suma es:", suma ) 
+
+#suma de palabras#
+palabra1 = "hola "
+palabra2 = "mundo"
+suma_palabras = palabra1 + palabra2
+print(suma_palabras)
+
+
+#resta de numero"
+
+res1 = 20 
+res2 = 7
+
+resta = res1 - res2
+print("el resultado de la resta es:", resta)
+
+#multiplicaion de números#
+
+mul1 = 7 
+mul2 = 13
+
+multiplicacion = mul1 * mul2
+
+print("el resultado de la multiplicación es:", multiplicacion)
+
+#division de numeros#
+
+div1 = 90 
+div2 = 3 
+
+division = div1 / div2
+
+print("el resultado de la division es:", division)
+
+
+
+resi= 14 // 6 
+print(resi)
